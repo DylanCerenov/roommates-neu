65e88341f2be354ab85318d36b15311458af3ea4
ForVic <victor.lakers0@gmail.com>
2022-02-13 13:39:07 -0500

    committed stuff

--- roommatesneu/backend/server.js
+++ roommatesneu/backend/server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+import roommates from './api/roommates.route.js';
 const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1/roommates", roommates);
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
+
+
 const port = process.env.PORT || 3001;
 // const uri = process.env.ATLAS_URI;
 
 // mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
-app.use(cors());
-app.use(bodyParser.json());
 
 app.listen(port, () => {
     console.log(`listening on port + ${port}`);
 })
+
+export default app;
