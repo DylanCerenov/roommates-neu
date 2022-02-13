755fa049c60ef52bb8ec8b253b42396877ec610c
ForVic <victor.lakers0@gmail.com>
2022-02-13 13:49:50 -0500

    stuff

--- roommatesneu/backend/server.js
+++ roommatesneu/backend/server.js
@@ -12,15 +12,6 @@ app.use(express.json());
 app.use("/api/v1/roommates", roommates);
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
 
-
 const port = process.env.PORT || 3001;
-// const uri = process.env.ATLAS_URI;
-
-// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-
-
-app.listen(port, () => {
-    console.log(`listening on port + ${port}`);
-})
 
 export default app;
