deb568573dfd0b591c3508fe8d82d53b783f753c
Dylan Cerenov <dcerenov25@gmail.com>
2022-03-27 13:21:02 -0400

    Added the profile creation page

--- roommatesneu/src/CreateProfilePage.js
+++ roommatesneu/src/CreateProfilePage.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Checkbox } from '@mui/material';
+import { FormGroup } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
+import { TextField } from '@mui/material';
 
 function CreateProfilePage() {
   return (
-    <div>CreateProfilePage</div>
+    <div classname="profileCreator">
+      <h1>RoommatesNEU</h1>
+      <h2>Profile Creator:</h2>
+
+      <TextField id="outlined-basic" label="Name" variant="outlined"/>
+
+      <h2>Preferences</h2>
+      <FormGroup>
+        <FormControlLabel control={<Checkbox/>} label="Morning Person"/>
+        <FormControlLabel control={<Checkbox/>} label="Overnight Guests"/>
+        <FormControlLabel control={<Checkbox/>} label="Out Late"/>
+        <FormControlLabel control={<Checkbox/>} label="Neat Freak"/>
+      </FormGroup>
+
+
+    </div>
   )
 }
 
