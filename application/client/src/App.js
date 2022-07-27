bafa9af94738dbcc35f76879775fbf78c314945c
Dylan Cerenov <dcerenov25@gmail.com>
2022-07-27 14:48:15 -0400

    Added search functionality to the table

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -12,11 +12,16 @@ function App() {
   const [extroverted, setExtroverted] = useState(false); 
   const [neat, setNeat] = useState(false);
 
+  // variables for searching for relevant roommates in the table 
+  const [searchFrequentStudier, setSearchFrequentStudier] = useState(false);
+  const [searchExtroverted, setSearchExtroverted] = useState(false);
+  const [searchNeat, setSearchNeat] = useState(false);
+
   useEffect(() => {
     Axios.get("http://localhost:3001/getUsers").then((response) => {
-      setListOfUsers(response.data);
+      setListOfUsers(response.data.sort(compatibilitySort));
     });
-  }, []);
+  }, [listOfUsers]);
 
   // Function that calls the createUser api endpoint with the data inputted from the user 
   const createUser = () => {
@@ -40,12 +45,64 @@ function App() {
     });
   }
 
+  // compares two user objects to the search requirements
+  function compatibilitySort(a, b) {
+
+    let numKeys = Object.keys(a).length - 2; 
+    let aComparisonValue = 0.0;
+    let bComparisonValue = 0.0; 
+
+    // User A similarities
+    if (a.frequentStudier === searchFrequentStudier) {
+      aComparisonValue = aComparisonValue + (1.00 / numKeys);
+    }
+
+    if (a.extroverted === searchExtroverted) {
+      aComparisonValue = aComparisonValue + (1.00 / numKeys);
+    }
+
+    if (a.neat === searchNeat) {
+      aComparisonValue = aComparisonValue + (1.00 / numKeys);
+    }
+
+    // User B similariies 
+    if (b.frequentStudier === searchFrequentStudier) {
+      bComparisonValue = bComparisonValue + (1.00 / numKeys);
+    }
+
+    if (b.extroverted === searchExtroverted) {
+      bComparisonValue = bComparisonValue + (1.00 / numKeys);
+    }
+
+    if (b.neat === searchNeat) {
+      bComparisonValue = bComparisonValue + (1.00 / numKeys);
+    }
+
+    if (aComparisonValue > bComparisonValue) {
+      return -1; 
+    }
+    return 1; 
+  }
+
   return (
     <div className="App">
 
       <div className="Title">
         <h1>RoommatesNEU</h1>
         <h2>A roommate search engine</h2>
+      </div>
+
+      <div className="SearchCriteria">
+        <h3>Enter your preferences below:</h3>
+
+        <input type="checkbox" id="lookingForStudy" onChange={(event) => { setSearchFrequentStudier(event.target.checked); }}/>
+        <label for="lookingForStudy"> I am looking for a roommate who tends to study frequently </label> <br></br>
+
+        <input type="checkbox" id="lookingForExtrovert" onChange={(event) => { setSearchExtroverted(event.target.checked); }}/>
+        <label for="lookingForExtrovert"> I am looking for a roommate who is generally outgoing </label> <br></br>
+
+        <input type="checkbox" id="lookingForNeat" onChange={(event) => { setSearchNeat(event.target.checked); }}/>
+        <label for="lookingForNeat"> I am looking for a roommate who is a neat person </label> <br></br>
       </div>
 
       <div className="UsersDisplay">
